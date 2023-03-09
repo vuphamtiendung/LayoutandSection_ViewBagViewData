f89985124890052c719f89c52b3fad11903de238
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2023-03-09 15:38:21 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLayout_003.Models;
+
+namespace MVCLayout_003.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Greeting"] = "Hello word";
+            ViewBag.Products = new ProductModel()
+            {
+                Id = 1,
+                Name = "Product 1",
+                Brand = "TEDU",
+                Price = 500000
+            };
+            return View();
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCLayout_003.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string Brand { get; set; }   
+        public double Price { get; set; }
+    }
+}
